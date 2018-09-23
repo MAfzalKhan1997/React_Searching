aa8fc62f1046b27affdcd7c7b2baaf84889a48e3
Muhammad Afzal Khan <MAfzalKhan1997@gmail.com>
2018-09-23 20:49:57 +0500

    Searching

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './screens/Search/Search'
 
 class App extends Component {
+
+  renderHeader() {
+    return (
+      <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Search</h1>
+      </header>
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
- 
-        </p>
+        {this.renderHeader()}
+        <Search />
       </div>
     );
   }
